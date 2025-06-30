c80b9ff251316ed852045064c055b39c6b63d2a4
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-06-30 19:05:45 +0500

    new new update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   ssr: {
-    noExternal: ['kaboom'],
+    noExternal: ['kaboom'], // kaboom'un dışardan optimize edilmesini engeller
   },
 })
