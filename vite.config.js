b8131008cec0f411fbe803292a066321dbc99e24
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-06-30 19:19:18 +0500

    fix: remove ignoreCommand

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  ssr: {
-    noExternal: ['kaboom'], // kaboom'un dışardan optimize edilmesini engeller
+  build: {
+    minify: false, // 💥 Minify KAPALI
+    target: "esnext", // ESM uyumlu
   },
-})
+  ssr: {
+    noExternal: ["kaboom"], // Kaboom optimize edilmesin
+  },
+});
