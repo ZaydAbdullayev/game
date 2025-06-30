a2f33d1916240e0d2699602ae83c308685a6d042
Zayyad <132747547+ZaydAbdullayev@users.noreply.github.com>
2025-06-30 18:14:49 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")).render(
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<App />} />
-        <Route path="/game" element={<Game />} />
+        <Route path="/game/:player" element={<Game />} />
       </Route>
     </Routes>
   </BrowserRouter>
